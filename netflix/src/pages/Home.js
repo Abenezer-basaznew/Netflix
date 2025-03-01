09deefdfecd2cccd93d9f5b1e1b36981c8a57265
Abenezer basaznew <abenezerbasazne.gmail.com>
2025-03-01 09:29:48 -0800

    add Banners javascript and css file

--- netflix/src/pages/Home.js
+++ netflix/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import Banner from '../components/Banner/Banner'
 
 
 function Home() {
   return (
-    <>
+    <>   
+        
         <Header />
-      
+        <Banner />
         <Footer />
     </>
   )
